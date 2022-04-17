39d3f46db2c5c5a524cf48d2400d3dc6cebff845
zaidyahya <zaid.yahya@mail.mcgill.ca>
2022-04-17 18:40:03 -0400

    Use index.html [5]

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 # Index.html, JS bundle, Fetch API on bundle, refactor dir structure to finalize
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('home.html')
