546319b81b0725c94d1c86fc6139968afd789f35
zaidyahya <zaid.yahya@mail.mcgill.ca>
2022-04-17 19:05:27 -0400

    Use bundle.js

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Index.html, JS bundle, Fetch API on bundle, refactor dir structure to finalize
+# Index.html, JS bundle, CSS on it, Fetch API on bundle, refactor dir structure to finalize
 @app.route("/")
 def index():
     return render_template('index.html')
